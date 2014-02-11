1076baa91ed04b01a441549e0a9c4dbc7e616c7f
Uira Kulesza <uirakulesza@gmail.com>
2014-02-11 23:44:41 +0100

    Mais um sysout

--- cloning-spls/src/br/ufrn/dimap/clone/NewTest.java
+++ cloning-spls/src/br/ufrn/dimap/clone/NewTest.java
@@ -5,6 +5,8 @@ public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world !!");
 		System.out.println("One more message !!");
+		System.out.println("Mais uma mensagem !!");
+		
 	}
 
 }
